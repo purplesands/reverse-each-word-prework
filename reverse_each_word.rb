1475fc88a1701b8a728bec95f388505a6bda0bc1
Joe Mullen <mullenjoe13@gmail.com>
2019-01-06 00:16:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  array.collect {|word| word.reverse}.join(" ")
+  array = sentence.split(" ") #turning string into an array
+  array.collect {|word| word.reverse}.join(" ") #reverses each word in array, then joins them into a new string
 end
